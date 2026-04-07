4df32771c5837701794e6e28c7c11cb0c9f78ef6
Vladimir Spirin <spirin.vladimir@gmail.com>
2026-04-07 20:35:58 +0700

    add test

--- test.js
+++ test.js
@@ -5,20 +5,15 @@ const {load} = require('./index')
 describe('sushiswap-dump', () => {
 
     it('Find pair addresses WBTC/WETH', () =>
-        load().then(pairs => {
-            const pair = pairs.find(_ =>
-                _.token0 == '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599' &&
-                _.token1 == '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
-            )
-            console.log(pair.pair)
+        load().then(pairs =>
             assert.ok(
                 pairs.some(_ =>
-                    //_.pair == '0x4ab6702b3ed3877e9b1f203f90cbef13d663b0e8' &&
+                    _.pair == '0xceff51756c56ceffca006cd410b03ffc46dd3a58' &&
                     _.token0 == '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599' &&
                     _.token1 == '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
                 )
             )
-        })
+        )
     )
 
 })
